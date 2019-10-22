bb766a6265a8755fe261e872b0b46afb660fdfbe
Dorian Henning <dorian.henning@gmail.com>
2019-10-22 14:19:59 +0100

    * modified cmake OpenCV search directory to find proper (ROS) install

--- build_ros.sh
+++ build_ros.sh
@@ -3,5 +3,5 @@ echo "Building ROS nodes"
 cd Examples/ROS/ORB_SLAM2
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
+cmake .. -DROS_BUILD_TYPE=Release -DOpenCV_DIR=/opt/ros/kinect/
 make -j
